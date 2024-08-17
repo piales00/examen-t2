dfc931c18c160f769e57524a12af3573946c303b
piales00 <156816944+piales00@users.noreply.github.com>
2024-08-17 09:06:29 -0500

    Primer cambio de la app
    estuve repasando un poco

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./assets/css/App.css";
+
+import MiComponente from "./components/MiComponente";
+import Pizza from "./components/Pizza";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -17,19 +20,17 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <section className="componentes">
+          <MiComponente />
+          <Pizza />
+        </section>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MiComponente.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MiComponente extends Component {
+  render() {
+    return <h1>Hola mundo</h1>;
+  }
+}
+
+export default MiComponente;
+
+//import React, { Component } from 'react'
+//imrc
+
+//rcc
